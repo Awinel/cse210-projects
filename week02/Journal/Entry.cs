fafb7d4edc8f38000948a85a00c1954129f2ca24
Awinel <benjamin.huerta.awi@gmail.com>
2025-07-14 20:46:06 -0400

    Add journal app with entry, prompt, and quote features

---
+++ week02/Journal/Entry.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Entry
+{
+    public string Date;
+    public string PromptText;
+    public string EntryText;
+
+    public Entry() { }
+
+    public Entry(string date, string promptText, string entryText)
+    {
+        Date = date;
+        PromptText = promptText;
+        EntryText = entryText;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Date: {Date}");
+        Console.WriteLine($"Prompt: {PromptText}");
+        Console.WriteLine($"Entry: {EntryText}");
+        Console.WriteLine();
+    }
+}
